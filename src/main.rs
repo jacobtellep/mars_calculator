5c502168ffaeecb282f662c68c5f003d74516801
Jake Tellep <jj.tellep@gmail.com>
2022-07-16 19:15:53 -0400

    finished calculator

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weight (kg): ");
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
-    let mut mars_weight = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
-    println!("Weight on Mars: {}g", mars_weight);
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight = input.trim().parse().unwrap();
+    let mars_weight = calculate_weight_on_mars(weight);
+
+    println!("Weight on Mars: {} kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
