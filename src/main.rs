b7b8986567452431047d2bf4d36cd67e917bb762
Jake Tellep <jj.tellep@gmail.com>
2022-07-16 17:25:25 -0400

    created first rust function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+
+    calculate_weight_on_mars(100.0);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    50.0 // you don't have to use return keyword and semicolon
 }
